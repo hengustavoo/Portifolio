c7ba3732e7c7a9370e35ca071a2aeed6f9d92982
Gustavo Valério <gustavocontatohenrique@gmail.com>
2025-04-08 08:43:00 -0300

    att

--- public/js/scripts.js
+++
@@ -1,9 +0,0 @@
-fetch('/api/projetos')
-  .then(res => res.json())
-  .then(data => console.log('Dados recebidos:', data))
-  .catch(err => console.error('Erro na API:', err))
-
-fetch('/api/recados')
-  .then(res => res.json())
-  .then(data => console.log('Dados recebidos:', data))
-  .catch(err => console.error('Erro na API:', err))
