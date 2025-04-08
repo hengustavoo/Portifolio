51bd01e150108f2fc0e3cc8b1c45f27b47720d75
Gustavo Valério <gustavocontatohenrique@gmail.com>
2025-04-08 10:51:32 -0300

    Script para fazer requisição do feed

---
+++ public/js/scripts.js
@@ -0,0 +1,35 @@
+(async function fetchProjetos() {
+  try {
+    const response = await fetch('https://mysql.devgustavo.com.br/api/projetos');
+    if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+    
+    const data = await response.json();
+    console.log('Dados recebidos:', data);
+  } catch (error) {
+    console.error('Erro ao buscar projetos:', error);
+  }
+})();
+
+(async function fetchRecados() {
+  try {
+    const response = await fetch('https://mysql.devgustavo.com.br/api/recados');
+    if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+    
+    const data = await response.json();
+    console.log('Dados recebidos:', data);
+  } catch (error) {
+    console.error('Erro ao buscar projetos:', error);
+  }
+})();
+
+(async function fetchRecados() {
+  try {
+    const response = await fetch('https://mysql.devgustavo.com.br/api/feed');
+    if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+    
+    const data = await response.json();
+    console.log('Dados recebidos:', data);
+  } catch (error) {
+    console.error('Erro ao buscar projetos:', error);
+  }
+})();
